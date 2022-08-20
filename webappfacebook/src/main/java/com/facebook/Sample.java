57168711482430bb75093b22e5559e0d4c0a3baf
vsndpanu <110671048+vsndpanu@users.noreply.github.com>
2022-08-20 09:40:27 +0530

    adding local files via upload
    webappfacebook

---
+++ webappfacebook/src/main/java/com/facebook/Sample.java
@@ -0,0 +1,13 @@
+package com.facebook;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sample 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven Sample World!" );
+    }
+}
